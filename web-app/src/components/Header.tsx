496c924e0b78238191991a0fffae8088c71c2cac
Bartolomeo Müller <bartolomeo.mueller@gmail.com>
2026-03-28 13:03:44 +0100

    Make header usable

--- web-app/src/components/Header.tsx
+++ web-app/src/components/Header.tsx
@@ -3,10 +3,10 @@ import { Home } from "lucide-react";
 
 export default function Header() {
   return (
-    <header className="flex items-center bg-gray-800 p-4 text-white shadow-lg">
+    <header className="flex items-center border-b border-border bg-background p-4 pb-2 text-foreground">
       <Link to="/" className="flex items-center gap-2">
-        <Home></Home>
-        <h1 className="text-xl font-semibold">Talent Match</h1>
+        <Home className="text-primary"></Home>
+        <h1 className="text-xl font-semibold text-primary">Talent Match</h1>
       </Link>
     </header>
   );
