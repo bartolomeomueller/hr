0163cd5ece7059041b3c88f2fbe9a67540675381
Bartolomeo Müller <bartolomeo.mueller@gmail.com>
2026-05-04 11:16:25 +0200

    Add new logo

--- web-app/src/components/layout/Header.tsx
+++ web-app/src/components/layout/Header.tsx
@@ -1,14 +1,14 @@
 import { Link } from "@tanstack/react-router";
-import { Home } from "lucide-react";
 import HeaderAccount from "@/components/layout/HeaderAccount";
+import Logo from "@/components/layout/Logo";
 
 export default function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b border-border bg-background/85 text-foreground backdrop-blur">
       <div className="flex items-center gap-4 px-4 py-2">
         <Link to="/" className="flex items-center gap-2">
-          <Home className="text-primary"></Home>
-          <h1 className="text-xl font-semibold text-primary">Talent Match</h1>
+          <Logo aria-hidden="true" className="size-10 text-primary" />
+          <h1 className="text-xl font-semibold text-primary">Hirephant</h1>
         </Link>
         <HeaderAccount />
       </div>
